2a598d3239e60ed3685964adfc7cfb509dabe5c3
Tiago <tf.prudente@gmail.com>
2017-05-08 18:07:14 -0300

    Override var with eval, can be harmful

---
+++ eval.js
@@ -0,0 +1,20 @@
+function foo(str, a) {
+	eval( str ); // cheating!
+	console.log( a, b );
+}
+
+var b = 2;
+
+foo( "var b = 3;", 1 ); // 1 3
+
+
+/*
+  This wil throw a referenceError and says a is not defined
+function foo(str) {
+   "use strict";
+   eval( str );
+   console.log( a ); // ReferenceError: a is not defined
+}
+
+foo( "var a = 2" );
+*/
