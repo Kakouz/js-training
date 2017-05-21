8e1e90fee07dd3026403317e7b4381bf6d8a11d2
Tiago <tf.prudente@gmail.com>
2017-05-21 11:01:54 -0300

    Finished greedy

--- pset1/greedy.js
+++ pset1/greedy.js
@@ -1,21 +1,30 @@
-function greedy() {
-  var num = document.getElementById('num').value; //Get the total to return to the client
-  var coins = 0;  //Calculate the amount of coins to return
+function check() {
+  var num = document.getElementById("num").value; //Get the change owed to the client
 
-while (num > 0) {
-  if (num >= 0.25) {
-    coins++;
-    num - 0.25;
-  } else if (num >= 0.10) {
-    coins++;
-    num - 0.10;
-  } else if (num >= 0.05) {
-    coins++;
-    num - 0.05;
-  } else if (num >= 0.01) {
-    coins++;
-    num - 0.01;
+  var c1 = .25; //All coins that will be used
+  var c2 = .10;
+  var c3 = .05;
+  var c4 = .01;
+
+  var coins = 0; //Total coins to return
+
+  while(num > 0.00) {
+    if (num >=c1) {
+      coins++;
+      num = num - c1;
+    }
+    else if (num >= c2) {
+      coins++;
+      num = num - c2;
+    }
+    else if (num >= c3) {
+      coins++;
+      num = num - c3;
+    }
+    else {
+      coins++;
+      num = num - c4;
+    }
   }
-}
   document.write("I gave you " + coins + " coins!");
 }
